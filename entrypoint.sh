6cb3006422adac0fa04a21d62853f0f1face2e27
Felipe Schottz <33692557+schottz@users.noreply.github.com>
2023-06-21 08:33:23 -0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+OLD_IFS=$IFS
+IFS=','
+for field in $HIDDEN_FIELDS; do echo $field >> /opt/fields-to-hide.txt; done
+IFS=$OLD_IFS
+
+#source vars if file exists
+DEFAULT=/etc/default/fluentd
+
+if [ -r $DEFAULT ]; then
+    set -o allexport
+    . $DEFAULT
+    set +o allexport
+fi
+
+# If the user has supplied only arguments append them to `fluentd` command
+if [ "${1#-}" != "$1" ]; then
+    set -- fluentd "$@"
+fi
+
+# If user does not supply config file or plugins, use the default
+if [ "$1" = "fluentd" ]; then
+    if ! echo $@ | grep ' \-c' ; then
+       set -- "$@" -c /fluentd/etc/${FLUENTD_CONF}
+    fi
+
+    if ! echo $@ | grep ' \-p' ; then
+       set -- "$@" -p /fluentd/plugins
+    fi
+fi
+
+exec "$@"
